554f917247ce7868d7571fc7dd211cf9378f6791
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2020-03-18 16:59:13 +0200

    implement get

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -79,4 +79,42 @@ class DoublyLinkedList {
     this.length++;
     return this;
   }
+
+  get(i) {
+    if (i < 0 || i >= this.length) return undefined;
+    let current;
+    let count = 0;
+    const mid = Math.floor(this.length / 2);
+
+    if (i <= mid) {
+      current = this.head;
+      while (count !== i) {
+        current = current.next;
+        count++;
+      }
+    } else {
+      current = this.tail;
+      count = this.length - 1;
+      while (count !== i) {
+        current = current.prev;
+        count--;
+      }
+    }
+    return current;
+  }
 }
+
+const list = new DoublyLinkedList();
+
+list
+  .push(0)
+  .push(1)
+  .push(2)
+  .push(3)
+  .push(4);
+
+// list.get(1);
+
+// console.log(list);
+
+console.log(list.get(4).value);
