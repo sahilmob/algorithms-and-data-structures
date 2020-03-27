fa170868b2337c2ac1c940cb63a2d1b2a5373d18
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2020-03-27 18:32:32 +0300

    implement depth first search

--- graph.js
+++ graph.js
@@ -28,4 +28,40 @@ class Graph {
     }
     delete this.adjacencyList[vertex];
   }
+
+  dfsRecursive(vertex) {
+    const result = [];
+    const visitedVertexes = {};
+    const adjacencyList = this.adjacencyList;
+
+    (function dfs(ver) {
+      if (!ver) return;
+      visitedVertexes[ver] = true;
+      result.push(ver);
+      adjacencyList[ver].forEach(element => {
+        if (!visitedVertexes[element]) return dfs(element);
+      });
+    })(vertex);
+
+    return result;
+  }
 }
+
+const g = new Graph();
+
+g.addVertex("A");
+g.addVertex("B");
+g.addVertex("C");
+g.addVertex("D");
+g.addVertex("E");
+g.addVertex("F");
+
+g.addEdge("A", "B");
+g.addEdge("A", "C");
+g.addEdge("B", "D");
+g.addEdge("C", "E");
+g.addEdge("D", "E");
+g.addEdge("D", "F");
+g.addEdge("E", "F");
+
+console.log(g.dfsRecursive("A"));
